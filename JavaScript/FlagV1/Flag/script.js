2119f801ba5c91ea1fcf6c51b4ecdb8a4d03510a
DileepKumarMantena <deelipkumar261997@gmail.com>
2024-09-05 21:59:53 +0530

    Added new version folder for flag

---
+++ JavaScript/FlagV1/Flag/script.js
@@ -0,0 +1,5 @@
+document.getElementById('flag').addEventListener('click', function() {
+  console.log('inside function')
+  document.getElementById('container').style.display='none'
+  document.getElementById('message').style.display='block'
+});
