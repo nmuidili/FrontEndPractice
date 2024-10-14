1774f7eeab6c0047e5727dc6026a16586488fb72
Narendra Muidili <narendra.techvander@outlook.com>
2024-10-14 12:38:16 -0400

    myfirst react app

---
+++ my-first-app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { useState,useEffect } from 'react';
+
+function App() {
+  const [count,setCount] = useState(0);
+  useEffect(() => {
+    alert(`Count: ${count}`);
+  });
+  return (
+    <div >
+      <button onClick={() => setCount(count + 1)}>Click</button>
+    </div>
+  );
+}
+
+export default App;
