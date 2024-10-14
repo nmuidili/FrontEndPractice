bf4b012a8127fcdee22f20d7deeb1f60e21addab
Narendra Muidili <narendra.techvander@outlook.com>
2024-10-14 12:53:20 -0400

    dummy log

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -5,6 +5,7 @@ import { useState,useEffect } from 'react';
 function App() {
   const [count,setCount] = useState(0);
   useEffect(() => {
+    console.log("dummy");
     alert(`Count: ${count}`);
   });
   return (
